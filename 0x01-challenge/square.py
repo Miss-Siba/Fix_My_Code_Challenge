dd8b8dd317a15c469e8875b6fddab1ee67f64ac9
Miss-Siba <sibaquma@gmail.com>
2024-04-07 11:31:55 -0700

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,21 +12,21 @@ class Square:
         """
         self.side_length = side_length
 
-    def area(self):
+    def area_of_my_square(self):
         """Calculate the area of the square."""
         return self.side_length ** 2
 
-    def perimeter(self):
+    def PermiterOfMySquare(self):
         """Calculate the perimeter of the square."""
         return self.side_length * 4
 
     def __str__(self):
         """Return a string representation of the square."""
-        return f"Square with side length {self.side_length}"
+        return "{}".format(self.side_length)
 
 
 if __name__ == "__main__":
-    square = Square(side_length=9)
-    print(square)
-    print("Area:", square.area())
-    print("Perimeter:", square.perimeter())
+    s = Square(side_length=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
