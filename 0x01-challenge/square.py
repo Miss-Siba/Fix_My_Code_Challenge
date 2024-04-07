2dd440c7af8de585aba6367751bb66fbe81e7db3
Miss-Siba <sibaquma@gmail.com>
2024-04-07 11:13:12 -0700

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+""" Defines a squre class """
+
 
 class square():
-    
-    side_length = 0
+    """Class representing a square."""
 
-    
     def __init__(self, side_length):
+        """
+        Initialize a Square instance with a given side length.
+
+        Args:
+            side_length (int): The length of each side of the square.
+        """
         self.side_length = side_length
 
     def area_of_my_square(self):
@@ -13,14 +19,27 @@ class square():
         return self.side_length ** 2
 
     def PermiterOfMySquare(self):
+        """
+        Calculate the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
         return self.side_length * 4
 
     def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string describing the square.
+        """
         return f"Square with side length {self.side_length}"
+
 
 if __name__ == "__main__":
 
-    s = square(side_length = 9)
+    s = square(side_length=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
