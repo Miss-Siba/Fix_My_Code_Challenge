52c01920b1c927bd01858a87a36557a20d18e32c
Miss-Siba <sibaquma@gmail.com>
2024-04-14 11:27:36 -0700

    challenge

---
+++ 0x01-challenge/blog/app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
+
+  # Add the 'online' boolean attribute with default value true
+  attribute :online, :boolean, default: true
+end
+
